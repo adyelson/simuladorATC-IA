bf6545823976cef3f4ea8f0c203228be768aa743
Felix <adyelson.loureiro@gmail.com>
2023-07-01 09:32:31 -0300

    voice improve1

--- speech.js
+++ speech.js
@@ -21,7 +21,12 @@ function startTalk(){
     recognition.addEventListener('result', (event) => {
         // Pega a primeira frase reconhecida
         const firstResult = event.results[0][0].transcript;
-        console.log(event)
+        let listaPalavras = firstResult.split();
+        array.forEach((element, index) => {
+           console.log(`${index}:${element}`) 
+        });       
+        
+        
         // Verifique a frase e execute um comando dependendo dela
         if (firstResult.includes('vetoração')) {
             console.log('curvando agora');
@@ -32,3 +37,60 @@ function startTalk(){
         }
     });
 }
+
+function obterLetraFonetica(palavra) {
+    var alfabetoFonetico = {
+      'Alfa': 'A',
+      'Bravo': 'B',
+      'Charlie': 'C',
+      'Delta': 'D',
+      'Eco': 'E',
+      'Foxtrot': 'F',
+      'Golfo': 'G',
+      'Hotel': 'H',
+      'Índia': 'I',
+      'Juliett': 'J',
+      'Kilo': 'K',
+      'Lima': 'L',
+      'Mike': 'M',
+      'November': 'N',
+      'Óscar': 'O',
+      'Papa': 'P',
+      'Quebec': 'Q',
+      'Romeo': 'R',
+      'Sierra': 'S',
+      'Tango': 'T',
+      'Uniforme': 'U',
+      'Vitor': 'V',
+      'Whiskey': 'W',
+      'X-ray': 'X',
+      'Yankee': 'Y',
+      'Zulu': 'Z'
+    };
+  
+    // Verifica se a palavra está presente no objeto
+    if (alfabetoFonetico.hasOwnProperty(palavra)) {
+      return alfabetoFonetico[palavra];
+    }
+  
+    // Verifica a tolerância para palavras similares
+    var palavrasToleradas = {
+      'yank': 'Yankee',
+      'echo': 'Eco',
+      'charli': 'Charlie'
+    };
+  
+    // Verifica se a palavra está presente nas palavras toleradas
+    if (palavrasToleradas.hasOwnProperty(palavra)) {
+      return alfabetoFonetico[palavrasToleradas[palavra]];
+    }
+  
+    // Caso nenhuma correspondência seja encontrada
+    return null;
+  }
+  
+  // Exemplo de uso
+  var palavra = 'yank';
+  var letraCorrespondente = obterLetraFonetica(palavra);
+  console.log(letraCorrespondente); // Saída: Y
+  

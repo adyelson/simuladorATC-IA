5688f9020b7ca523f149a73ab41fca8c2a0afe05
Felix <adyelson.loureiro@gmail.com>
2023-07-03 10:41:33 -0300

    mudancaVEl ok

--- aircraft.js
+++ aircraft.js
@@ -7,27 +7,24 @@ export default class Aircraft {
     this.arrLocation = arrLocation;
     this.flightLevel = flightLevel;
     this.speed = speed;
+    this.direction = direction;
     const { x, y } = this.convertLatLongToXY(lat, long);
     const { velX, velY } = this.calculateVelocities(speed, direction);
     this.updatePosition(x, y);
-    this.updateVelocity(velX, velY);
-
+    this.velX = velX;
+    this.velY = velY;
     this.labelX = this.x + 40;
     this.labelY = this.y - 50;
   }
 
-   convertLatLongToXY(lat, long) {
+  convertLatLongToXY(lat, long) {
     const latOrigin = 0; // Latitude de origem (00° 00' 00'')
     const longOrigin = 0; // Longitude de origem (000° 00' 00'')
-
     const latitudeInRadians = (lat - latOrigin) * (Math.PI / 180);
     const longitudeInRadians = (long - longOrigin) * (Math.PI / 180);
-
     const earthRadius = 6371; // Raio médio da Terra em km
-
     const x = earthRadius * longitudeInRadians;
     const y = earthRadius * Math.log(Math.tan(Math.PI / 4 + latitudeInRadians / 2));
-
     return { x, y };
   }
 
@@ -35,33 +32,20 @@ export default class Aircraft {
   calculateVelocities(speed, direction) {
     // Converter a direção de graus para radianos
     const directionInRadians = (direction - 90) * (Math.PI / 180);
-  
     // Calcular as velocidades nas direções x e y
     const velX = speed * Math.cos(directionInRadians);
     const velY = speed * Math.sin(directionInRadians);
   
     return { velX, velY };
   }
-  
+
   updatePosition(x, y) {
     this.x = x;
     this.y = y;
-
     this.positions.push({ x: this.x, y: this.y });
-
     if (this.positions.length > 9) {
       this.positions.shift();
     }
-  }
-
-  updateVelocity(velX, velY) {
-    this.velX = velX;
-    this.velY = velY;
-
-    // Calcula e armazena o rumo magnético em graus em relação aos rumos geográficos
-    this.direction = Math.round(Math.atan2(this.velY, this.velX) * (180 / Math.PI));
-    this.direction = this.direction % 360;
-    this.direction = (this.direction + 90) % 360;
   }
 
   move() {
@@ -72,30 +56,33 @@ export default class Aircraft {
     this.updatePosition(this.x, this.y);
   }
 
- 
-  mudarVelocidade(novaVelocidade) {
-    
-    const anguloRumo = (this.direction * Math.PI) / 180;
-    const velocidadeXAtual = this.speed * Math.cos(anguloRumo);
-    const velocidadeYAtual = this.speed * Math.sin(anguloRumo);
 
-    const velocidadesNovas = this.calculateVelocities(novaVelocidade, this.direction);
+  mudarVelocidade(newSpeed) {
+    let intervalId = setInterval(() => {
+      if (this.speed > newSpeed) {
+        this.speed -= this.acceleration;       
+        if (this.speed - newSpeed <= 0) {
+          this.speed = newSpeed;
+        }        
+      } else if (this.speed < newSpeed) {
+        
+        this.speed += this.acceleration;
+        if (newSpeed - this.speed <= 0) {
+          this.speed = newSpeed;
+        }       
+      } else {
+        clearInterval(intervalId); // Encerra o setInterval quando a velocidade atinge o novo valor desejado
+      }
+      let velocities = this.calculateVelocities(this.speed, this.direction);
+        this.velX = velocities.velX;
+        this.velY = velocities.velY;
+    }, 1000);
     
-    let velocidadeX = velocidadeXAtual;
-    let velocidadeY = velocidadeYAtual;
 
-    let velocidadeXNova = velocidadesNovas['velX'];
-    let velocidadeYNova = velocidadesNovas['velY'];
-    
-
-    
-
-    this.velX = velocidadeXNova;
-    this.velY = velocidadeYNova;
   }
 
- 
- 
+
+
 }
 
 
